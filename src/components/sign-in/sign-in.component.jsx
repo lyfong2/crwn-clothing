3e815982aecc069c2d42ab8e3b0a17185a7318de
Danny Lin <dannyjabez@gmail.com>
2020-11-14 20:22:11 -0500

    finishing sign in handleSubmit method

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import {auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 import './sign-in.style.scss';
 
@@ -15,11 +15,18 @@ class SignIn extends React.Component{
         }
     }
 
-    handleSubmit = event => {
-        // 按下submit button, 將不會送出,及換頁的動作
+    handleSubmit = async event => {
         event.preventDefault();
-        this.setState({ email: '', password: '' });
-    }
+
+        const { email, password } = this.state;
+
+        try {
+            await auth.signInWithEmailAndPassword(email, password); 
+            this.setState({ email: '', password: '' });
+        } catch (error){
+            console.log(error);
+        }
+    };
 
     handleChange = event => {
         const { value, name } = event.target;
